786c2fef86ff1e5c01df07d96733dba4d1cb10a4
yk4663 <108866240+yk4663@users.noreply.github.com>
2022-09-25 15:25:06 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const data = 'data';
+
+const aTag = document.querySelector('username');
+aTag.addEventListener('click', () => {
+  location.href = `selProduct.html?${data}`;
+});
+
+const username = 
+{
+    name: "username",
+}
+  
+  const session = setAttribute.querySelector('a');
+  aTag.addEventListener('click', () => {
+    request.setItem("username", JSON.stringify(username));
+  });
+
+const receivedData = location.href.split('/Login.hrml')[1];
+console.log(receivedData); // data
+
+// 전달받은 데이터가 한글일 경우
+console.log(decodeURI(receivedData));
+
+const ("username") = session.setAttribute("username", request.getParameter("username"));
+localStorage.remove("username");
+console.log(name); // 아이디 변수 넘기기
+
+
+const seeion = document.querySelector('username');
+aTag.addEventListener('click', () => {
+  location.href = `selProduct.html?${data}`;
+});
+
+
+ArrayList.list = session.getAttribute("productList");
+if(list == Null)
+{
+    out.printIn("선택상품이 없습니다")
+}
+
+else
+{
+    for ( 0; item ; list)
+    out.printIn(item + "<br />");
+}
+
+const receivedData = request.getParameter("product");
+
+ArrayList.list  = session.getArraylist(productList);
+    if(list==null) 
+    
+    {
+        list = new Arraylist();
+        session.setAttribute(productList, list);
+    }
+
+    list.add(productName);
